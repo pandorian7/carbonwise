8edb3c2d919db7e3ca24cd8e245aabdc93733961
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-06-20 11:15:59 +0530

    added tab hover effect

--- src/Components/ui/Tab.jsx
+++ src/Components/ui/Tab.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function Tab({ title = "Tab", active = false, count = 0 }) {
   return (
     <div
-      className={`px-4 py-6 flex justify-center items-center ${count ? "gap-2": ""} ${
-        active ? "border-b-[3px] border-white" : ""
+      className={`px-4 py-6 flex justify-center items-center group transition duration-150 ${count ? "gap-2": ""} ${
+        active ? "border-b-[3px] border-white" : "hover:border-b-[1px] hover:border-white"
       }`}
     >
-      <div className={`justify-start ${active ? "text-white text-base font-semibold": "text-base-muted-foreground text-sm font-normal"} font-['Plus_Jakarta_Sans'] leading-tight`}>
+      <div className={`justify-start ${active ? "text-white text-base font-semibold": "text-base-muted-foreground text-sm font-normal group-hover:text-white transition duration-150"} font-['Plus_Jakarta_Sans'] leading-tight`}>
         {title}
       </div>
-      {count && (
+      {count ? (
         <div
           data-state="Default"
           data-variant="Secondary"
@@ -20,7 +20,7 @@ function Tab({ title = "Tab", active = false, count = 0 }) {
             {count}
           </div>
         </div>
-      )}
+      ): null}
     </div>
   );
 }
